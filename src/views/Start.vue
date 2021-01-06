51cf0ab78f09b35cc746d6ea0ec23f0264bef265
richardso21 <richiso899@yahoo.com>
2021-01-05 19:05:09 -0500

    layout views and components (pt 2)

---
+++ src/views/Start.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Welcome to the JavaScript Quiz Game!</h1>
+    <p><i>How well do you really know JavaScript?</i></p>
+    <br>
+    <router-link to="/Quiz" tag="button" id="startButton">Testing</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: 2.5rem;
+}
+p {
+  font-size: 1.75rem;
+}
+#startButton {
+  font-size: 1.25rem;
+}
+</style>
